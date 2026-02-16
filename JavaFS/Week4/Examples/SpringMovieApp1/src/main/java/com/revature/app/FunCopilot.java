44355e60e490f50dafc4e7883c83180cdac36a74
RomanLatipov <61292132+RomanLatipov@users.noreply.github.com>
2026-02-16 00:50:06 -0500

    Add files via upload

---
+++ JavaFS/Week4/Examples/SpringMovieApp1/src/main/java/com/revature/app/FunCopilot.java
@@ -0,0 +1,12 @@
+package com.revature.app;
+
+public class FunCopilot {
+
+    public boolean isitEven(int num) {
+        if (num % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
